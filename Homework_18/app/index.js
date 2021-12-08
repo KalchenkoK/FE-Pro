1b0019b7a64c9098aba9ef17fdbaec486a81f7f4
KalchenkoK <7584663@gmail.com>
2021-12-09 00:39:23 +0200

    homework_18

---
+++ Homework_18/app/index.js
@@ -0,0 +1,55 @@
+const form = document.forms.form;
+const emailValue = form.email;
+const passwordValue = form.password;
+const submitButton = form.submitButton;
+const state = {};
+
+const validation = {
+  email: (value) => !value.includes("@"),
+  password: (value, mail) =>
+    value.length < 9 || value.length > 24 || value === mail.email,
+  passwordConfirm: (value, rest) => rest.password !== value,
+  consent: (checked) => !checked,
+};
+
+const errors = {
+  email: true,
+  password: true,
+  passwordConfirm: true,
+  consent: true,
+};
+
+const handleEvent = (event) => {
+  const { type, name, value, checked } = event.target;
+  switch (type) {
+    case "checkbox":
+      state[name] = checked;
+      break;
+
+    default:
+      state[name] = value;
+      break;
+  }
+
+  errors[name] =
+    name in validation ? validation[name](state[name], state) : false;
+  event.currentTarget.submitButton.disabled = Object.keys(errors).some(
+    (key) => errors[key]
+  );
+};
+
+const handleFocusOut = (event) => {
+  event.currentTarget.submitButton.disabled = !state.consent;
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const outTag = document.createElement("output");
+
+  outTag.innerHTML = `email: ${emailValue.value}, password: ${passwordValue.value}`;
+  form.after(outTag);
+};
+form.addEventListener("focusout", handleEvent);
+form.addEventListener("submit", handleSubmit);
+// submitButton.addEventListener('click', () => {
+// 	form.requestSubmit();
+// });
